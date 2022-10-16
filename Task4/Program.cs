ea2a2b6cb9f61a3d5a29fe550066528f6034449d
Darya <darya.novosadova@gmail.com>
2022-10-16 21:53:07 +0700

    add Task 4 max of three numbers

---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int [] number = {numberA, numberB, numberC};
+int size = 3;
+int index = 0;
+int max = number[0];
+while(index < size)
+{
+    if(number[index] > max)
+    {
+        max = number[index];
+    }
+    index++;
+}
+Console.WriteLine($"MAX = {max}");
